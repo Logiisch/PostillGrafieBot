e70afa7c5c77c86900eec479456e8411996726af
Fabian Ferrari <fabian.ferrari@rwth-aachen.de>
2018-05-21 00:12:09 +0200

    fixes

--- src/main/java/util/SECRETS.java
+++ src/main/java/util/SECRETS.java
@@ -17,22 +17,13 @@ import java.util.List;
 public class SECRETS {
 
     private static String getToken() {
-        if (Main.Testlauf) {
-            try {
-                List<String> in = readInTxtFile.Read("../tokenT.txt");
-                return in.get(0);
-            } catch (Exception e) {
-                e.printStackTrace();
-                return "";
-            }
-        } else {
         try {
-           List<String> in = readInTxtFile.Read("../tokenP.txt");
-           return in.get(0);
+            List<String> in = readInTxtFile.Read("../tokenP.txt");
+            return in.get(0);
         } catch (Exception e) {
             e.printStackTrace();
             return "";
-        } }
+        }
     }
 
     private static String getTwitterApiKey() {
